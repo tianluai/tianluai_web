4453b616ebd23852ea4a21610a46c361a4e08a7b
kayiganwa04 <kayiganwa04@gmail.com>
2026-03-19 19:11:26 +0200

    feat: added devops ci/cd pipelines

--- next.config.ts
+++ next.config.ts
@@ -5,7 +5,7 @@ import createNextIntlPlugin from "next-intl/plugin";
 const withNextIntl = createNextIntlPlugin("./src/i18n/request.ts");
 
 const nextConfig: NextConfig = {
-  /* config options here */
+  output: "standalone",
 };
 
 const sentryOptions = {
